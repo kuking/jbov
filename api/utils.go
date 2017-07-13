f55c417c39596909892f3903c9495454de650130
Eduardo ES Riccardi <ed@kukino.uk>
2017-07-13 16:40:49 +0200

    utils impl and testing

--- api/utils.go
+++ api/utils.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"crypto/rand"
 	"github.com/kuking/jbov/api/md"
-	"strings"
 	"errors"
+	"regexp"
 )
 
 func generateUniqid(prefix string) string {
@@ -36,28 +36,39 @@ func IsVolumeUniqId(uniqid *string) bool {
 	return true
 }
 
+func isValidCname(cname *string) bool {
+	re := regexp.MustCompile("^[a-z0-9_]{3,20}$") //TODO: compile just once?
+	return re.MatchString(*cname)
+}
 
-func CanCreate(jbov *md.JBOV) error {
+func IsValidJBOV(jbov *md.JBOV) (bool, error) {
 
-	if strings.Trim(jbov.Cname, " \t\n\\l") == "" {
-		return errors.New("JBOV Cname can't be empty")
+	if !isValidCname(&jbov.Cname) {
+		return false, errors.New("JBOV cname is not valid")
 	}
 	if !IsJbovUniqId(&jbov.Uniqid) {
-		return errors.New("JBOV UniqId does not looks valid")
+		return false, errors.New("JBOV UniqId does not looks valid")
 	}
-
 	if &jbov.Volumes == nil || len(jbov.Volumes) == 0 {
-		return errors.New("JBOV requires at least one Volumen")
+		return false, errors.New("JBOV requires at least one volume")
+	}
+	for cname, vol := range jbov.Volumes {
+		if !isValidCname(&cname) {
+			return false, errors.New("JBOV volume has an invalid cname")
+		}
+		if !IsVolumeUniqId(&vol.Uniqid) {
+			return false, errors.New("Volume uniqid does not looks valid")
+		}
+
 	}
 
-
-	return nil
+	return true, nil
 }
 
 func Create(jbov *md.JBOV) error {
 
-	err := CanCreate(jbov)
-	if err != nil {
+	ok, err := IsValidJBOV(jbov)
+	if !ok && err != nil {
 		return err
 	}
 
--- api/utils_test.go
+++ api/utils_test.go
@@ -3,7 +3,10 @@ package api
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"github.com/kuking/jbov/api/md"
 )
+
+// uniq ids creation and validation
 
 func TestGeneratedJbovUniqIdValidates(t *testing.T) {
 	uniqid := GenerateJbovUniqId()
@@ -25,3 +28,85 @@ func TestGenerateVolumeUniqIdDoesNotValidateAsJBovId(t *testing.T) {
 	assert.False(t, IsJbovUniqId(&jbovUniqId), "GenerateVolumeUniqId should not be validated by IsJbovUniqId")
 }
 
+// cname validation
+
+func TestValidCname(t *testing.T) {
+	valids := []string{"valid", "valvalval", "long_valid", "lowercase_is_ok", "vol1", "vol", "numbers_ok_111"}
+	for _, valid := range valids {
+		assert.True(t, isValidCname(&valid), "'" + valid + "' should be a valid cname")
+	}
+}
+
+func TestNotValidCname(t *testing.T) {
+	invalids := []string{"", "AA", "aa", "UPPERCASE_NOT_OK", "super_long_one_is_not_valid"}
+	for _, invalid := range invalids {
+		assert.False(t, isValidCname(&invalid), "'" + invalid + "' should be an invalid cname")
+	}
+}
+
+func TestValidCname_InvalidSymbols(t *testing.T) {
+	all_invalids := "!@£$%^&*-=~`[]{}();:'\",./<>?j"
+	for _, c := range all_invalids {
+		st := string(c)
+		assert.False(t, isValidCname(&st), "'" + st + "' should not be a valid cname")
+	}
+}
+
+
+// integrated IsValidJBOV
+
+func TestCanCreate_happyPath(t *testing.T) {
+	jbov := givenAValidJBOV()
+
+	ok, err := IsValidJBOV(&jbov)
+
+	assert.True(t, ok)
+	assert.NoError(t, err, "this JBOV should be valid")
+}
+
+func TestCanCreate_invalidCname(t *testing.T) {
+	jbov := givenAValidJBOV()
+	jbov.Cname = "INVALID"
+
+	ok, err := IsValidJBOV(&jbov)
+
+	assert.False(t, ok)
+	assert.EqualErrorf(t, err, "JBOV cname is not valid", "invalid cname should fail")
+}
+
+func TestCanCreate_invalidJBovUniqId(t *testing.T) {
+	jbov := givenAValidJBOV()
+	jbov.Uniqid = "invalid"
+
+	ok, err := IsValidJBOV(&jbov)
+
+	assert.False(t, ok)
+	assert.EqualErrorf(t, err, "JBOV UniqId does not looks valid", "invalid jbov uniq should fail")
+}
+
+func TestCanCreate_noVolumes(t *testing.T) {
+	jbov := givenAValidJBOV()
+	jbov.Volumes = nil
+
+	ok, err := IsValidJBOV(&jbov)
+
+	assert.False(t, ok)
+	assert.EqualErrorf(t, err, "JBOV requires at least one volume", "jbov without volumes should fail")
+}
+
+func TestCanCreate_VolumeWithInvalidName(t *testing.T) {
+	jbov := givenAValidJBOV()
+	jbov.Volumes["INVALID_VOL_CNAME"] = jbov.Volumes["vol1"]
+
+	ok, err := IsValidJBOV(&jbov)
+
+	assert.False(t, ok)
+	assert.EqualErrorf(t, err, "JBOV volume has an invalid cname", "volume with invalid cname should fail")
+}
+
+func givenAValidJBOV() md.JBOV {
+	vols := make(map[string]md.Volume)
+	vols["vol1"]=md.Volume{Uniqid: GenerateVolumeUniqId(), LastMountPoint: "/mnt/vol1", Deprecated: false }
+	vols["vol2"]=md.Volume{Uniqid: GenerateVolumeUniqId(), LastMountPoint: "/mnt/vol2", Deprecated: false }
+	return md.JBOV{Cname: "valid", Uniqid: GenerateJbovUniqId(), Volumes: vols}
+}
